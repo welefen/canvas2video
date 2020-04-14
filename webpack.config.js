21530bf29428422f762e7049a6a52985d2b26a07
welefen <welefen@gmail.com>
2020-04-14 09:46:03 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     extensions: [ '.ts', '.js' ]
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'canvas2video.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
   },
